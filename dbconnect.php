ef14b90498430456ba0d42fdc2a3436f45cb6a29
Egor Ryzhenko <102794788+yaproklayt@users.noreply.github.com>
2022-05-08 22:37:24 +0500

    Add files via upload

---
+++ dbconnect.php
@@ -0,0 +1,11 @@
+<?php
+// подключение к БД
+try {
+    $conn = new PDO("mysql:host=localhost;dbname=gloevk", 'root', '');
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+}
+catch(PDOException $e) {
+    echo "Ошибка подключения к БД: " . $e->getMessage(), $e->getCode( );
+    die();
+}
